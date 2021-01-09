ee86a609818960d4aea436c42193db9f079dcf13
Wyean Chan <chwyean@gmail.com>
2021-01-09 18:32:08 +0800

    Load data file in App.componentDidMount() method

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import './App.css';
 import DisplayOptionControl from './DisplayOptionControl'
 import MainWindow from './MainWindow';
 
+//import Papa from './PapaParse/papaparse';
+import Papa from 'papaparse';
+
+
 const hideOptions = {
   "symbol":"symbol",
   "pinyin":"pinyin",
   "none":"none"
 };
 
+const dataFilePath = 'vocab-raw-test.csv';
+const localDataFilePath = 'vocab-raw-test.csv';
+
 class App extends React.Component {
+
+  wordData=null;
 
   constructor(props){
     super(props)
@@ -18,13 +27,17 @@ class App extends React.Component {
       hideOption:"symbol"
     };
   }
-
+ 
   handleHideOptionChange = (option) => {
     this.setState((state) => ({
       ...state,
       hideOption: option
     }))
   };
+
+  componentDidMount() {
+    this.readDataFile();
+  }
 
   render() {
     return (
@@ -44,6 +57,48 @@ class App extends React.Component {
       </div>
     );
   }
+
+  // load CSV data
+  loadData() {
+    Papa.parse(dataFilePath, {
+      download: true,
+      complete: function(results) {
+        console.log(results);
+      }
+    });
+  }
+
+  loadDataText(text) {
+    console.log("text:\n" + text)
+    // const results = Papa.parse(text, {
+    //   header:true
+    // });
+    // console.log(results);
+  }
+
+  printHello() {
+    console.log("hello");
+  }
+
+  readDataFile() {
+    var xmlhttp = new XMLHttpRequest();
+    const that = this;
+    xmlhttp.onreadystatechange = function(){
+      if(xmlhttp.status === 200 && xmlhttp.readyState === 4){
+        const txt = xmlhttp.responseText;
+        console.log("Read file:" + localDataFilePath)
+
+        that.printHello();
+        that.loadDataText(txt);
+      }
+    };
+    
+    //xmlhttp.open("GET","vocab-raw.csv",true);
+    xmlhttp.open("GET",localDataFilePath,true);
+    xmlhttp.send();
+  }
 }
+
+
 
 export default App;
